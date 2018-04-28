2e897c39c1ed675ca489330aa1a02b9197178cf6
andrzejgorski <andrzejgorski@supermond.com>
2018-04-28 14:07:37 +0200

    Remove old implementation.

--- neural_network.py
+++ neural_network.py
@@ -2,16 +2,6 @@ from collections import deque
 import numpy as np
 
 
-endl = '\n'
-
-
-def sigmoid(x, derivative=False):
-    if (derivative == True):
-        return x * (1 - x)
-    else:
-        return 1 / (1 + np.exp(-x))
-
-
 def softmax(x, derivative=False):
     if not derivative:
         return np.exp(x) / float(sum(np.exp(x)))
@@ -19,89 +9,8 @@ def softmax(x, derivative=False):
     return np.diag(x) - np.dot(sm, sm.T)
 
 
-def ReLU(x, derivative=False):
-    if not derivative:
-        return np.array([max(0, y) for y in x])
-    return np.array([1 if y > 0 else 0 for y in x])
-
-
-def np_init():
-    np.random.seed(1)
-
-
-alpha = 0.1
-
-
-# def _stack_ones(layer_input):
-#     ones = np.ones((layer_input.shape[0], 1))
-#     return np.hstack((ones, layer_input))
-
-
-def _stack_ones(layer_input):
-    ones = np.ones((layer_input.shape[0], 1))
-    return np.hstack((layer_input, ones))
-
-
-class NeutralNetworkCalculation(object):
-    def __init__(self, neutral_network, dataset):
-        self.dataset = dataset
-        self.neutral_network = neutral_network
-        self._weights = neutral_network._weights
-        self.layer_outputs = None
-
-    def run(self):
-        self.layer_outputs = [self.dataset[0]]
-        for layer in range(len(self._weights)):
-            stack_ones = _stack_ones(self.layer_outputs[layer])
-            next_layer = np.dot(stack_ones, self._weights[layer])
-
-            if layer == len(self._weights) - 1:
-                self.final_output = next_layer
-
-            self.layer_outputs.append(sigmoid(next_layer))
-        return self.final_output
-
-
-class BackPropagation(NeutralNetworkCalculation):
-    def __init__(self, neutral_network, dataset):
-        self.dataset = dataset
-        self.neutral_network = neutral_network
-        self._weights = neutral_network._weights
-        self.layer_outputs = None
-
-    def _calc_layer_errors(self, output_error):
-        self._layer_errors = [output_error]
-        last_error_layer = output_error
-        for layer_ix in reversed(range(len(self.layer_outputs[: -1]))):
-            sigmoid_derivative = sigmoid(
-                self.layer_outputs[layer_ix], derivative=True)
-
-            # output_error in new implementaion
-            weights_derivative = np.dot(
-                last_error_layer, self._weights[layer_ix].T[:, 1:])
-
-            last_error_layer = sigmoid_derivative * weights_derivative
-            # last error is mid_error in new implementation
-            self._layer_errors.append(last_error_layer)
-
-        self._layer_errors.reverse()
-
-    def _calc_gradients(self):
-        self.gradients = []
-        for index in range(len(self.layer_outputs) - 1):
-            formatted_output = _stack_ones(self.layer_outputs[index])[:, :, np.newaxis]
-            formatted_error = self._layer_errors[index + 1][: , np.newaxis, :]
-            partial_derivatives = formatted_output * formatted_error
-            total_gradient = np.average(partial_derivatives, axis=0)
-            self.gradients.append(total_gradient)
-
-    def run(self):
-        output_error = super(BackPropagation, self).run() - self.dataset[1]
-        self._calc_layer_errors(output_error)
-        self._calc_gradients()
-
-
 class Weights(object):
+    endl = '\n'
     def __init__(self, weights=None):
         self._weights = weights or []
 
@@ -110,11 +19,11 @@ class Weights(object):
 
     def save_weights(self, filename):
         with open(filename, 'w') as f:
-            f.write(str(len(self._weights)) + endl)
+            f.write(str(len(self._weights)) + self.endl)
             for weight in self._weights:
-                f.write('{} {}'.format(len(weight), len(weight[0])) + endl)
+                f.write('{} {}'.format(len(weight), len(weight[0])) + self.endl)
                 for row in weight:
-                    f.write(' '.join(str(f) for f in row) + endl)
+                    f.write(' '.join(str(f) for f in row) + self.endl)
 
     def load_weights(self, filename):
         self._weights = []
@@ -131,76 +40,6 @@ class Weights(object):
                     list_input.append(new_row)
 
                 self._weights.append(np.array(list_input))
-
-    def swap(self):
-        for ix in range(len(self._weights)):
-            tmp = deque(self._weights[ix])
-            tmp.rotate(-1)
-            self._weights[ix] = np.array(tmp)
-
-
-
-class NeutralNetwork(object):
-    def __init__(self, layers):
-        self.layers = layers
-
-    def save_weights(self, filename):
-        with open(filename, 'w') as f:
-            f.write(str(len(self._weights)) + endl)
-            for weight in self._weights:
-                f.write('{} {}'.format(len(weight), len(weight[0])) + endl)
-                for row in weight:
-                    f.write(' '.join(str(f) for f in row) + endl)
-
-    def load_weights(self, filename):
-        self._weights = []
-        with open(filename, 'r') as f:
-            layers = int(f.readline())
-            for _ in range(layers):
-                list_input = []
-                rows, columns = (int(v) for v in f.readline().split(' '))
-                for __ in range(rows):
-                    new_row = np.array(
-                        [float(numb) for numb in  f.readline().split(' ')],
-                        dtype=np.float64
-                    )
-                    list_input.append(new_row)
-
-                self._weights.append(np.array(list_input))
-
-    def create_weights(self):
-        self._weights = []
-        for inn, out in zip(self.layers[:-1], self.layers[1:]):
-            self._weights.append(2 * np.random.random((inn + 1, out)) - 1)
-
-    def _T_without_first_col(self, array):
-        return array.T[:, 1: ]
-
-    def learn(self, dataset):
-        backp = BackPropagation(self, dataset)
-        backp.run()
-        for index in range(len(self._weights)):
-            self._weights[index] -= alpha * backp.gradients[index]
-
-    def calc(self, dataset):
-        self.calc = NeutralNetworkCalculation(self, dataset)
-        return self.calc.run()
-
-
-def create_simple_dataset():
-    X = np.array([
-        # [0, 0, 1],
-        # [0, 1, 1],
-        # [1, 0, 0],
-        # [1, 1, 0],
-        # [1, 0, 1],
-        [1, 1, 1],
-    ])
-
-    # y = np.array([[0, 1, 0, 1, 1, 0]]).T
-    y = np.array([[0]]).T
-    return (X, y)
-
 
 
 def sigmoid2(input_, output_, derivative=False):
@@ -219,7 +58,12 @@ def id_(input_, output_, derivative=False):
     else:
         for i in range(len(output_)):
             output_[i][i] = 1
-            # output_[i][i] = input_[i]
+
+
+def ReLU(x, derivative=False):
+    if not derivative:
+        return np.array([max(0, y) for y in x])
+    return np.array([1 if y > 0 else 0 for y in x])
 
 
 def matrix_mult(first, second, out, first_r, second_c, first_c,
@@ -390,23 +234,8 @@ class NeutralNetworkLayered(object):
         return self.layers[-1].output_value
 
 
-nn = NeutralNetwork([3, 3, 1])
-nn.load_weights('weights.txt')
-dataset = create_simple_dataset()
-nn.learn(dataset)
-
-weights = Weights()
-weights.load_weights('weights.txt')
-nnl = NeutralNetworkLayered([3, 3, 1])
-nnl.feed_weights(weights)
-nnl.learn(dataset)
-
-
+# Move to tests
 def check_equal(arr1, arr2):
     for row1, row2 in zip(arr1, arr2):
         for cell1, cell2 in zip(row1, row2):
             assert abs(cell1 - cell2) < 0.000001
-
-
-check_equal(nnl.layers[0].weights, nn._weights[0].T)
-check_equal(nnl.layers[1].weights, nn._weights[1].T)
