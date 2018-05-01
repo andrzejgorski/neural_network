7f8511353283837c4361d67c1d8c68e167f28eb7
andrzejgorski <andrzejgorski@supermond.com>
2018-05-01 07:43:06 +0200

    Add mid_values calculating.

--- neural_network.cc
+++ neural_network.cc
@@ -48,15 +48,22 @@ class Layer {
     protected:
     int input_size, output_size;
     double alpha;
-    matrix mid_value, mid_error, output_value, output_error;
+    matrix mid_value, mid_error;
     matrix gradients;
 
-    void init ();
     virtual void activation_function() {};
 
     public:
+    matrix output_value, output_error;
     matrix weights;
-    Layer (int i_size, int o_size): input_size{i_size}, output_size{o_size} {};
+    Layer (int i_size, int o_size): input_size{i_size}, output_size{o_size} {
+        mid_value = create_matrix(output_size, 1);
+        mid_error = create_matrix(output_size, 1);
+        output_value = create_matrix(output_size + 1, 1, 1);
+        output_error = create_matrix(output_size, 1);
+        weights = create_matrix(output_size, input_size + 1);
+        gradients = create_matrix(output_size, input_size + 1);
+    }
     virtual void calc() {};
     virtual void print();
     void set_correct_output(matrix outputs) {
@@ -65,64 +72,6 @@ class Layer {
         }
     };
 };
-
-
-class FirstReLULayer:public Layer {
-    protected:
-    matrix input_value;
-    public:
-    FirstReLULayer (int, int);
-    void print ();
-    void calc ();
-    void init_input(matrix input) {
-        input_value = input;
-    }
-    void backpropagation ();
-};
-
-
-void FirstReLULayer::calc() {
-    matrix_mult(
-        weights,
-        input_value,
-        mid_value,
-        output_size,
-        1,
-        input_size + 1
-    );
-
-    for (int i = 0; i < output_size; i++) {
-        if (mid_value[i][0] > 0) {
-            output_value[i][0] = mid_value[i][0];
-        } else {
-            output_value[i][0] = 0;
-        }
-    }
-}
-
-
-void FirstReLULayer::backpropagation() {
-    for (int i = 0; i < output_size; i++) {
-        if (mid_value[i][0] >= 0) {
-            mid_error[i][0] = mid_value[i][0] * output_error[i][0];
-        } else {
-            mid_error[i][0] = 0;
-        }
-    }
-    matrix_mult_ts(
-        mid_error,
-        input_value,
-        gradients,
-        output_size,
-        input_size + 1,
-        1
-    );
-    for (int i = 0; i < output_size; i ++) {
-        for (int j = 0; j < input_size + 1; j ++) {
-            weights[i][j] -= 0.1 * gradients[i][j];
-        }
-    }
-}
 
 
 void Layer::print() {
@@ -165,14 +114,71 @@ void Layer::print() {
 }
 
 
-FirstReLULayer::FirstReLULayer (int i_size, int o_size):
-        Layer (i_size, o_size) {
-    input_value = create_matrix(input_size + 1, 1, 1);
-    init();
+class FirstLayer:public Layer {
+    protected:
+    matrix input_value;
+    public:
+    FirstLayer (int, int);
+    void print ();
+    void calc ();
+    void init_input(matrix input) {
+        input_value = input;
+    }
+    void backpropagation ();
+};
+
+
+void FirstLayer::calc() {
+    matrix_mult(
+        weights,
+        input_value,
+        mid_value,
+        output_size,
+        1,
+        input_size + 1
+    );
+
+    for (int i = 0; i < output_size; i++) {
+        if (mid_value[i][0] > 0) {
+            output_value[i][0] = mid_value[i][0];
+        } else {
+            output_value[i][0] = 0;
+        }
+    }
 }
 
 
-void FirstReLULayer::print() {
+void FirstLayer::backpropagation() {
+    for (int i = 0; i < output_size; i++) {
+        if (mid_value[i][0] >= 0) {
+            mid_error[i][0] = mid_value[i][0] * output_error[i][0];
+        } else {
+            mid_error[i][0] = 0;
+        }
+    }
+    matrix_mult_ts(
+        mid_error,
+        input_value,
+        gradients,
+        output_size,
+        input_size + 1,
+        1
+    );
+    for (int i = 0; i < output_size; i ++) {
+        for (int j = 0; j < input_size + 1; j ++) {
+            weights[i][j] -= 0.1 * gradients[i][j];
+        }
+    }
+}
+
+
+FirstLayer::FirstLayer (int i_size, int o_size):
+        Layer (i_size, o_size) {
+    input_value = create_matrix(input_size + 1, 1, 1);
+}
+
+
+void FirstLayer::print() {
     Layer::print();
     cout << "input_value: " << endl;
     for (int i = 0; i < input_size + 1; i ++) {
@@ -182,20 +188,64 @@ void FirstReLULayer::print() {
 }
 
 
-// Layer::Layer (int i_size, int o_size, double alp, Layer& prev):
-//         input_size{i_size}, output_size{o_size}, alpha{alp}, previous{&prev} {
-//     init();
-// }
+// MidLayer
 
 
-void Layer::init () {
-    mid_value = create_matrix(output_size, 1);
-    mid_error = create_matrix(output_size, 1);
-    output_value = create_matrix(output_size + 1, 1, 1);
-    output_error = create_matrix(output_size, 1);
-    weights = create_matrix(output_size, input_size + 1);
-    gradients = create_matrix(output_size, input_size + 1);
+class MidLayer:public Layer {
+    protected:
+    Layer &previous;
+    public:
+    MidLayer (int i_size, int o_size, Layer &prev): Layer(i_size, o_size), previous{prev} {};
+    void calc();
+    void backpropagation();
+};
+
+
+void MidLayer::calc() {
+    matrix_mult(
+        weights,
+        previous.output_value,
+        mid_value,
+        output_size,
+        1,
+        input_size + 1
+    );
+
+    for (int i = 0; i < output_size; i++) {
+        if (mid_value[i][0] > 0) {
+            output_value[i][0] = mid_value[i][0];
+        } else {
+            output_value[i][0] = 0;
+        }
+    }
 }
+
+
+void MidLayer::backpropagation() {
+    for (int i = 0; i < output_size; i++) {
+        if (mid_value[i][0] >= 0) {
+            mid_error[i][0] = mid_value[i][0] * output_error[i][0];
+        } else {
+            mid_error[i][0] = 0;
+        }
+    }
+    matrix_mult_ts(
+        mid_error,
+        previous.output_value,
+        gradients,
+        output_size,
+        input_size + 1,
+        1
+    );
+    for (int i = 0; i < output_size; i ++) {
+        for (int j = 0; j < input_size + 1; j ++) {
+            weights[i][j] -= 0.1 * gradients[i][j];
+        }
+    }
+}
+
+
+// Loading function
 
 
 void read_weigts(ifstream& file_stream, Layer& layer, int rows, int columns) {
@@ -207,39 +257,21 @@ void read_weigts(ifstream& file_stream, Layer& layer, int rows, int columns) {
 }
 
 
-class ReLULayer:public Layer {
-    protected:
-    Layer *previous;
-    public:
-    void activation_function() {
-        for (int i = 0; i < output_size; i ++) {
-            double tmp = mid_value[0][i];
-            if (tmp < 0) {
-                tmp = 0;
-            }
-            output_value[0][i] = tmp;
-        }
-    };
-    // ReLULayer(int i_size, int o_size, Layer& prev):
-    //     Layer(i_size, o_size, prev) {};
-};
-
-
-// Layer load(ifstream& file_stream, Layer& prev_layer) {
-//     int rows, columns;
-//     file_stream >> rows;
-//     file_stream >> columns;
-//     Layer new_layer = ReLULayer(rows, columns - 1, prev_layer);
-//     read_weigts(file_stream, new_layer, rows, columns);
-//     return new_layer;
-// }
-
-
-FirstReLULayer load (ifstream& file_stream) {
+MidLayer load (ifstream& file_stream, Layer &prev) {
     int rows, columns;
     file_stream >> rows;
     file_stream >> columns;
-    FirstReLULayer new_layer = FirstReLULayer(rows, columns);
+    MidLayer new_layer = MidLayer(columns, rows, prev);
+    read_weigts(file_stream, new_layer, rows, columns);
+    return new_layer;
+}
+
+
+FirstLayer load (ifstream& file_stream) {
+    int rows, columns;
+    file_stream >> rows;
+    file_stream >> columns;
+    FirstLayer new_layer = FirstLayer(columns, rows);
     read_weigts(file_stream, new_layer, rows, columns);
     return new_layer;
 }
@@ -247,16 +279,22 @@ FirstReLULayer load (ifstream& file_stream) {
 
 int main()
 {
-    ifstream file_stream;
-    file_stream.open("test.in");
-    FirstReLULayer ll = load(file_stream);
-    // ll.init_input(create_matrix(4, 1, -1));
-    ll.calc();
-    ll.set_correct_output(create_matrix(3, 1, 1));
-    ll.print();
-    ll.backpropagation();
-    ll.print();
+    ifstream in_stream;
+    in_stream.open("test.in");
+    FirstLayer first_layer = load(in_stream);
+    // first_layer.init_input(create_matrix(4, 1, -1));
+    first_layer.calc();
+    first_layer.print();
+    first_layer.set_correct_output(create_matrix(3, 1, 1));
+    first_layer.backpropagation();
 
-    file_stream.close();
+    ifstream mid_stream;
+    mid_stream.open("test_mid.in");
+    MidLayer mid_layer = load(mid_stream, first_layer);
+    mid_layer.calc();
+    mid_layer.backpropagation();
+    mid_layer.print();
+    in_stream.close();
+    mid_stream.close();
     return 0;
 }
